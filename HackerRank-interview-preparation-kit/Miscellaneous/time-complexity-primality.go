5943732413d897a032cd180bff2d0aaa5adc0d3b
Ilya A. Pimenov <ilya.pimenov@gmail.com>
2022-08-23 10:36:46 +0200

    Another two problems from Hacker Rank.

---
+++ HackerRank-interview-preparation-kit/Miscellaneous/time-complexity-primality.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'primality' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER n as parameter.
+ */
+
+func primality(n int32) string {
+	if n == 1 {
+		return "Not prime"
+	}
+	k := int32(math.Sqrt(float64(n)))
+	for i := int32(2); i <= k; i++ {
+		if n%i == 0 {
+			return "Not prime"
+		}
+	}
+	return "Prime"
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	pTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	p := int32(pTemp)
+
+	for pItr := 0; pItr < int(p); pItr++ {
+		nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		result := primality(n)
+
+		fmt.Fprintf(writer, "%s\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
