c32a09701f7d8a6e5e793ebfd9d1f1ceb0683b75
Ilya A. Pimenov <ilya.pimenov@gmail.com>
2022-08-31 18:33:26 +0200

    +1 trivia exercise

---
+++ Cracking-the-Coding-Interview/Chapter-5/e5_5.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+
+		(n & (n-1) == 0)
+
+		1011011101
+		1011011100
+		1011011100
+
+		101110
+		101101
+		101100
+
+		100000
+		011111
+		000000
+
+		I believe it checks that there is only one 1 in the bit value of n, thus is n is 2^k, k in [0...]
+
+	*/
+	fmt.Println("vim-go")
+}
