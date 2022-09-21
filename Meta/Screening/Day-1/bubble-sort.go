92b1db54f47d2f676d4f1e723af3c04983bc97ab
Ilya A. Pimenov <ilya.pimenov@gmail.com>
2022-09-21 14:43:04 +0200

    Implemented bubble sort as a practice exercise.

---
+++ Meta/Screening/Day-1/bubble-sort.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	/* more naive
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if arr[i] < arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	*/
+}
+
+func main() {
+	arr := []int{3, 123, 5, 23, 10}
+	fmt.Printf("Before: \n%v\n", arr)
+	bubbleSort(arr)
+	fmt.Printf("After: \n%v\n", arr)
+}
